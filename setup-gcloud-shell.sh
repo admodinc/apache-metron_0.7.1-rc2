632250e81a2ac997831847efe47a58f67f918da2
Shanu Koyakutty <shanu@admod.com>
2020-05-20 18:10:17 +0530

    Update for Cloud Shell environment

--- setup-gcloud-shell.sh
+++ setup-gcloud-shell.sh
@@ -8,3 +8,10 @@ sudo dpkg -i vagrant_2.2.9_x86_64.deb
 rm vagrant_2.2.9_x86_64.deb
 
 vagrant plugin install vagrant-google vagrant-hostmanager vagrant-cachier
+
+ssh-keygen -f ~/.ssh/id_rsa -q -N ""
+
+echo -e "++++++++++++++++++++++++++++\n\n\n"
+echo "Your public key to copy to Compute Engine SSH Metadata"
+
+cat ~/.ssh/id_rsa
