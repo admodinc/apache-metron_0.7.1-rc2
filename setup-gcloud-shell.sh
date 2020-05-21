09a5c101765b17d755fa3fdadb1a6aef881e0d3e
Shanu Koyakutty <shanu@admod.com>
2020-05-21 11:55:04 +0530

    Improve readability

--- setup-gcloud-shell.sh
+++ setup-gcloud-shell.sh
@@ -1,17 +1,24 @@
 #!/etc/bin/env bash
 
-sudo pip2 install ansible==2.6.5
+sudo apt install rsync && \
+sudo pip2 install ansible==2.6.5 && \
 
-wget https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-sudo dpkg -i vagrant_2.2.9_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb && \
+sudo dpkg -i vagrant_2.2.9_x86_64.deb && \
 
-rm vagrant_2.2.9_x86_64.deb
+rm vagrant_2.2.9_x86_64.deb && \
 
-vagrant plugin install vagrant-google vagrant-hostmanager vagrant-cachier
+vagrant plugin install vagrant-google vagrant-hostmanager vagrant-cachier && \
 
-ssh-keygen -f ~/.ssh/id_rsa -q -N ""
+RSA_FILE=~/.ssh/id_rsa
+
+if [ ! -f "$RSA_FILE" ]; then
+    ssh-keygen -f ~/.ssh/id_rsa -q -N ""
+fi
 
 echo -e "++++++++++++++++++++++++++++\n\n\n"
-echo "Your public key to copy to Compute Engine SSH Metadata"
+echo "RSA public key to be copied over to Compute Engine SSH Metadata"
+echo -e "----------------------------\n\n"
 
 cat ~/.ssh/id_rsa.pub
+echo -e "\n\n----------------------------"
